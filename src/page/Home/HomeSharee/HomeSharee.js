bcddb65fdc3db2f3cc9dce03a6ed7588ad886ded
hasansharif819 <hs.sharif819@gmail.com>
2022-04-24 03:15:28 +0600

    responsive for mobile

--- src/page/Home/HomeSharee/HomeSharee.js
+++ src/page/Home/HomeSharee/HomeSharee.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
 
-const HomeSharee = ({singleSharee}) => {
-    const {name, img, price} = singleSharee;
+const HomeSharee = ({ singleSharee }) => {
+    const { name, img, price } = singleSharee;
     return (
-        <div className='singleSharee col-sm-12 col-md-6 col-lg-4'>
-            <img height={300} width={300} src={img} alt="" />
-            <h4>{name}</h4>
-            <p>Price: ${price}</p>
+        <div className='col-sm-12 col-md-6 col-lg-4'>
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src={img} />
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>Price: ${price}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
         </div>
     );
 };
